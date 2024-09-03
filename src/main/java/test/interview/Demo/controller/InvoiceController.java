b3bc0d8bd4ad0ee9b95a641848260c8c6a22c4f9
Taylor Thurman <taylor.thurman@protonmail.com>
2024-09-03 09:39:18 -0500

    Added in UUID validity check

--- src/main/java/test/interview/Demo/controller/InvoiceController.java
+++ src/main/java/test/interview/Demo/controller/InvoiceController.java
@@ -7,19 +7,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import test.interview.demo.domain.Invoice;
+import test.interview.demo.exception.BadUserRequestException;
 import test.interview.demo.service.InvoiceService;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/invoice")
+@RequestMapping("/invoices")
 @RequiredArgsConstructor
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Invoice> getInvoiceById(@PathVariable UUID id) {
-        return ResponseEntity.ok(invoiceService.getInvoice(id));
+    public ResponseEntity<Invoice> getInvoiceById(@PathVariable String id) {
+        UUID invoiceId;
+        try {
+            invoiceId = UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            throw new BadUserRequestException("ID is not a valid UUID");
+        }
+        return ResponseEntity.ok(invoiceService.getInvoice(invoiceId));
     }
 }
